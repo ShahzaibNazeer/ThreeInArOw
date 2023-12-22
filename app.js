4fd7554b86b19fd057efe3b43632b02a4864d487
ShahzaibNazeer <125994398+ShahzaibNazeer@users.noreply.github.com>
2023-12-22 18:50:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log(`three in a row`);
+// change turn
+let turn=`x`;
+const changeTurn = () =>{
+    return turn ===`x`?`0`:`x`
+}
+
+// function print(){
+//     console.log(`hello`);
+// }
+// print();
+
+// const print1= function (){
+//     console.log (`hello`);
+// }
+// print1();
+
+// const print2 =()=> console.log(`hello`); 
+// print2();
